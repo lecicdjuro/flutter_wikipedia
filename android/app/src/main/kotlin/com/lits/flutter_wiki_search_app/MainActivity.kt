be2747d06b6af2b401897ebe3bfa54179ef8cea4
lecicdjuro <lecicdjuro@gmail.com>
2019-10-19 08:12:51 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/lits/flutter_wiki_search_app/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.lits.flutter_wiki_search_app
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
